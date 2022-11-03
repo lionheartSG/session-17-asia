c8526f3c220b40d73da20c7df006e74eb24343fe
urvashi29 <urvashigolu29@gmail.com>
2022-11-04 00:18:17 +0530

    first commit

---
+++ src/module/About/container/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+
+    render() {
+        return (
+            <div>
+            This is about page
+            </div>
+        );
+    }
+}
+
+
+
+export default About;
---
+++ src/reducer.js
@@ -0,0 +1,12 @@
+
+
+const initState = {
+    employee: []
+}
+
+const reducer = (state = initState, action) => {
+    return state;
+
+}
+
+export default reducer;
